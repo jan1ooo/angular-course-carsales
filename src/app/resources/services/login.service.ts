53822afe7919840fff8b5224bebaccfc4362c276
jan1ooo <filhojanio67@gmail.com>
2022-06-19 02:25:54 -0300

    Criando alertas

---
+++ src/app/resources/services/login.service.ts
@@ -0,0 +1,16 @@
+import { RequestLogin } from './../models/RequestLogin';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ResponseLogin } from '../models/ResponseLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  constructor(private httpClient: HttpClient) { }
+
+  public doLogin(requestLogin: RequestLogin): Observable<ResponseLogin> {
+    return this.httpClient.post<ResponseLogin>('http://localhost:8080/api/login', requestLogin)
+  }
+}
